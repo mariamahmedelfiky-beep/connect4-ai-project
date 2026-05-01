937b30ba3b0ed330b5743eb2147ab2dfea50521c
Ahmed el-fiky <mariam.ahmed.elfiky@gmail.com>
2026-05-01 15:38:48 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,17 @@
+ROWS = 6
+COLS = 7
+
+PLAYER = 1
+AI = 2
+
+EMPTY = 0
+
+WINDOW_LENGTH = 4
+
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+SQUARESIZE = 100
+RADIUS = int(SQUARESIZE / 2 - 5)
