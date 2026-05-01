c14140d553324d4639af7bb139d8134bfb6b4a8e
Ahmed el-fiky <mariam.ahmed.elfiky@gmail.com>
2026-05-01 16:48:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import random
 from gui import run_game
 
 
-if __name__ == "__main__":
+def start_game():
+
+    print("=" * 40)
+    print("        CONNECT 4 AI GAME")
+    print("=" * 40)
+    print("Human Player VS AI")
+    print("Game is starting...")
+    print("=" * 40)
+
     run_game()
+
+
+if __name__ == "__main__":
+    start_game()
